26873de8e9f8f8a26500568c8b553b27f167afbf
aytmamatov_musa <black.nobody.anb@gmail.com>
2022-07-23 19:49:39 +0600

    Implemented cell selection.

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,6 +2,7 @@ import { ExcelComponent } from '@/core/ExcelComponent';
 import { createTable } from '@/components/table/table.template';
 import { resizeHandler } from '@/components/table/table.resize';
 import { shouldResize } from '@/components/table/table.functions';
+import { TableSelection } from '@/components/table/TableSelection';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -11,6 +12,16 @@ export class Table extends ExcelComponent {
       listeners: ['mousedown'],
     });
   }
+
+  init() {
+    super.init();
+
+    this.selection = new TableSelection();
+    const $cell = this.$root.find('[data-id="1:1"]');
+    this.selection.select($cell);
+  }
+
+  prepare() {}
 
   onMousedown(event) {
     if (shouldResize) {
